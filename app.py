43ae654fb323534b7df14ea97cc9a75fd0a80952
Nathan <NateMateX2Z@outlook.com>
2026-09-05 12:43:54 +0100

    'app.py creation'

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st,st.set_page_config(
+    page_title="Students Who Slip On Maths",
+    page_icon="",
+    layout="wide,"
+)
+
+st.title("Students Who Slip On Maths")
+st.subheader("A dive into factors affecting students' performance in mathematics")
+
+st.write(
+    """
+This dashboard presents the results found in investigating which acadmemic factors
+most affect a student's final performance in mathematics. The data was recorded and collected 
+from Portuguese secondary school students across two different schools.
+
+Use the navigation menu on the left to explore the dashboard and and learn more about 
+the findings of this investigation.
+"""
+)
